ba70465ee6e78375e38fe8d8f1a558c53e441b56
vizitiuRoman <romanv@readcast.net>
2020-10-11 22:55:02 +0300

    Testing user model

--- pkg/models/user-model_test.go
+++ pkg/models/user-model_test.go
@@ -6,6 +6,13 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func clearDatabase(t *testing.T) {
+	_, err := db.Query("DELETE FROM users")
+	if err != nil {
+		t.Errorf("Cannot delete * from users error: %v", err)
+	}
+}
+
 func TestVerifyHashedPassword(t *testing.T) {
 	password := "password"
 	hashedPassword, err := hashPassword(password)
@@ -45,25 +52,111 @@ func TestValidateUser(t *testing.T) {
 }
 
 func TestCreateUser(t *testing.T) {
+	defer clearDatabase(t)
 
+	user := users[0]
+	createdUser, err := user.Create()
+	if err != nil {
+		t.Errorf("Cannot create user error: %v", err)
+	}
+	assert.Equal(t, user.Email, createdUser.Email)
+	assert.NotEmpty(t, createdUser.ID)
+	assert.NotEmpty(t, createdUser.Password)
 }
 
-func TestDeleteByIDUser(t *testing.T) {
+func TestDeleteUserByID(t *testing.T) {
+	defer clearDatabase(t)
 
+	user := users[0]
+	createdUser, err := user.Create()
+	if err != nil {
+		t.Errorf("Cannot create user error: %v", err)
+	}
+	err = user.DeleteByID(createdUser.ID)
+	assert.Nil(t, err)
 }
 
 func TestFindAllUsers(t *testing.T) {
+	defer clearDatabase(t)
 
+	var user User
+	var samples = []User{
+		{
+			Email:    "email@gmail.com",
+			Password: "password",
+		},
+		{
+			Email:    "email2@gmail.com",
+			Password: "password",
+		},
+		{
+			Email:    "email3@gmail.com",
+			Password: "password",
+		},
+		{
+			Email:    "email4@gmail.com",
+			Password: "password",
+		},
+	}
+
+	for _, sample := range samples {
+		_, err := sample.Create()
+		if err != nil {
+			t.Errorf("Cannot create user error: %v", err)
+		}
+	}
+	users, err := user.FindAll()
+	if err != nil {
+		t.Errorf("Cannot find all users error: %v", err)
+	}
+	assert.Equal(t, len(*users), 4)
 }
 
 func TestFindUserByID(t *testing.T) {
+	defer clearDatabase(t)
 
+	user := users[0]
+	createdUser, err := user.Create()
+	if err != nil {
+		t.Errorf("Cannot create user error: %v", err)
+	}
+
+	foundUser, err := user.FindByID(createdUser.ID)
+	if err != nil {
+		t.Errorf("Cannot find user error: %v", err)
+	}
+	assert.Equal(t, user.Email, foundUser.Email)
+	assert.Equal(t, createdUser.ID, foundUser.ID)
 }
 
 func TestFindUserByEmail(t *testing.T) {
+	defer clearDatabase(t)
 
+	user := users[0]
+	createdUser, err := user.Create()
+	if err != nil {
+		t.Errorf("Cannot create user error: %v", err)
+	}
+
+	foundUser, err := user.FindByEmail(createdUser.Email)
+	if err != nil {
+		t.Errorf("Cannot find user error: %v", err)
+	}
+	assert.Equal(t, user.Email, foundUser.Email)
+	assert.Equal(t, createdUser.ID, foundUser.ID)
 }
 
 func TestUpdateUser(t *testing.T) {
+	defer clearDatabase(t)
 
+	user := users[0]
+	createdUser, err := user.Create()
+	if err != nil {
+		t.Errorf("Cannot create user error: %v", err)
+	}
+
+	err = user.Update(createdUser.ID)
+	if err != nil {
+		t.Errorf("Cannot update user error: %v", err)
+	}
 }
